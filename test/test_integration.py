0260e601ca6c88d145ed1e5cabd3a3f7976a5ed1
stephane brossier <sbrossier@groupon.com>
2020-01-06 15:22:42 -0800

    Regenerate python client for 0.22.x release
    Fix broken integration test and verify it now passes

--- test/test_integration.py
+++ test/test_integration.py
@@ -32,6 +32,7 @@ from __future__ import absolute_import
 import unittest
 
 import killbill
+from killbill.configuration import Configuration
 from killbill.models.account import Account
 from killbill.models.tenant import Tenant
 from killbill.models.payment_method import PaymentMethod
@@ -58,35 +59,38 @@ class TestIntegration(unittest.TestCase):
         # Create tenant
         random_api_key = ''.join(choice(ascii_lowercase) for i in range(4))
         random_api_secret = ''.join(choice(ascii_lowercase) for i in range(5))
-        tenant = killbill.api.TenantApi()
+
+        config = Configuration()
+        config.api_key['X-Killbill-ApiKey'] = random_api_key
+        config.api_key['X-Killbill-ApiSecret'] =  random_api_secret
+
+        api_tenant = killbill.api.TenantApi()
         body = Tenant(api_key=random_api_key, api_secret=random_api_secret)
-        tenant.create_tenant(body, 'test')
+        api_tenant.create_tenant(body, 'test')
 
         # Get tenant
-        tenant = killbill.api.TenantApi()
-        tenant = tenant.get_tenant_by_api_key(api_key=random_api_key)
+        tenant = api_tenant.get_tenant_by_api_key(api_key=random_api_key)
         self.assertIsNotNone(tenant.tenant_id)
         self.assertEqual(random_api_key, tenant.api_key)
 
         # Upload XML catalog/Fetch XML catalog
-        catalog = killbill.api.CatalogApi()
+        api_catalog = killbill.api.CatalogApi()
         xml_catalog = open("../resources/SpyCarBasic.xml", "r+").read()
-        catalog.upload_catalog_xml(xml_catalog, 'test', random_api_key, random_api_secret)
+
+        api_catalog.upload_catalog_xml(xml_catalog, 'test')
 
         # Get catalog
-        catalog = killbill.api.CatalogApi()
-        catalog = catalog.get_catalog_xml(random_api_key, random_api_secret)
+        catalog = api_catalog.get_catalog_xml()
         self.assertIsNotNone(catalog)
 
         # Create account
-        account = killbill.api.AccountApi()
+        api_account = killbill.api.AccountApi()
         random_external_key = ''.join(choice(ascii_lowercase) for i in range(6))
         body = Account(name='John', external_key=random_external_key, currency='USD', state='CA', country='USA')
-        account.create_account(body, 'test', random_api_key, random_api_secret)
+        api_account.create_account(body, 'test')
 
         # Get account
-        account = killbill.api.AccountApi()
-        account = account.get_account_by_key(random_external_key, random_api_key, random_api_secret)
+        account = api_account.get_account_by_key(random_external_key)
         account_id = account.account_id
         self.assertIsNotNone(account.account_id)
         self.assertEqual(random_external_key, account.external_key)
@@ -96,116 +100,97 @@ class TestIntegration(unittest.TestCase):
         self.assertEqual('USA', account.country)
 
         # Add a payment method
-        account = killbill.api.AccountApi()
         body = PaymentMethod(plugin_name='__EXTERNAL_PAYMENT__', plugin_info=None)
-        account.create_payment_method(account_id, body, 'test', random_api_key, random_api_secret)
+        api_account.create_payment_method(account_id, body, 'test')
 
         # Get a payment method
-        account = killbill.api.AccountApi()
-        payment_method = account.get_payment_methods_for_account(account_id, random_api_key, random_api_secret)
+        payment_method = api_account.get_payment_methods_for_account(account_id)
         self.assertIsNotNone(payment_method[0].payment_method_id)
         self.assertEqual(account_id, payment_method[0].account_id)
         self.assertEqual('__EXTERNAL_PAYMENT__', payment_method[0].plugin_name)
 
         # Tag account as AUTO_INVOICING_OFF
-        account = killbill.api.AccountApi()
         tag = ["00000000-0000-0000-0000-000000000002"]
-        account.create_account_tags(account_id, tag, 'test', random_api_key, random_api_secret)
+        api_account.create_account_tags(account_id, tag, 'test')
 
         # Get account tags
-        tags = account.get_account_tags(account_id, random_api_key, random_api_secret)
+        tags = api_account.get_account_tags(account_id)
         self.assertIsNotNone(tags[0].tag_id)
         self.assertEqual("00000000-0000-0000-0000-000000000002", tags[0].tag_definition_id)
         self.assertEqual("AUTO_INVOICING_OFF", tags[0].tag_definition_name)
 
         # Create a subscription against plan
-        subscription = killbill.api.SubscriptionApi()
+        api_subscription = killbill.api.SubscriptionApi()
         body = Subscription(account_id=account_id, plan_name='standard-monthly')
-        subscription.create_subscription(body, 'test', random_api_key, random_api_secret)
+        api_subscription.create_subscription(body, 'test')
 
         # Get account bundles
-        account = killbill.api.AccountApi()
-        bundles = account.get_account_bundles(account_id, random_api_key, random_api_secret)
+        bundles = api_account.get_account_bundles(account_id)
         subscription_id = bundles[0].subscriptions[0].subscription_id
 
         # Get subscription
-        subscription = killbill.api.SubscriptionApi()
-        subscription = subscription.get_subscription(subscription_id, random_api_key, random_api_secret)
+        subscription = api_subscription.get_subscription(subscription_id)
         self.assertEqual('standard-monthly', subscription.plan_name)
 
         time.sleep(.5)
 
         # Get account invoices
-        account = killbill.api.AccountApi()
-        invoices = account.get_invoices_for_account(account_id, random_api_key, random_api_secret)
+        invoices = api_account.get_invoices_for_account(account_id)
         self.assertEqual([], invoices)
 
         # Remove AUTO_INVOICING_OFF tag
-        account = killbill.api.AccountApi()
-        account.delete_account_tags(account_id, 'test', random_api_key, random_api_secret, tag_def=tag)
+        api_account.delete_account_tags(account_id, 'test', tag_def=tag)
 
         time.sleep(.5)
 
         # Get account invoices
-        account = killbill.api.AccountApi()
-        invoices = account.get_invoices_for_account(account_id, random_api_key, random_api_secret)
+        invoices = api_account.get_invoices_for_account(account_id)
         self.assertIsNotNone(invoices[0].invoice_id)
 
         # Create a dryRun invoice
-        invoice = killbill.api.InvoiceApi()
+        api_invoice = killbill.api.InvoiceApi()
         body = InvoiceDryRun(dry_run_type='TARGET_DATE')
-        invoice.generate_dry_run_invoice(body, account_id, 'test', random_api_key, random_api_secret)
+        api_invoice.generate_dry_run_invoice(body, account_id, 'test')
 
         # Modify Plan
-        subscription = killbill.api.SubscriptionApi()
-        body = Subscription(subscription_id=subscription_id, product_name='Super', billing_period='MONTHLY',
-                            price_list='DEFAULT')
-        subscription.change_subscription_plan(subscription_id, body, 'test', random_api_key, random_api_secret,
-                                              billing_policy='IMMEDIATE')
+        body = Subscription(subscription_id=subscription_id, plan_name='super-monthly')
+        api_subscription.change_subscription_plan(subscription_id, body, 'test')
 
         # Create external charge
-        invoice = killbill.api.InvoiceApi()
         body = InvoiceItem(account_id=account_id, amount=50, currency='USD', description='My charge')
-        invoice.create_external_charges(account_id, [body], 'test', random_api_key, random_api_secret, auto_commit=True)
+        api_invoice.create_external_charges(account_id, [body], 'test', auto_commit=True)
 
         # Verify account balance
-        account = killbill.api.AccountApi()
-        account = account.get_account(account_id, random_api_key, random_api_secret, account_with_balance=True)
+        account = api_account.get_account(account_id, account_with_balance=True)
         self.assertEqual(50.0, account.account_balance)
 
         # Pay all unpaid invoices
-        account = killbill.api.AccountApi()
-        account.pay_all_invoices(account_id, 'test', random_api_key, random_api_secret, external_payment=True)
+        api_account.pay_all_invoices(account_id, 'test', external_payment=True)
 
         # Verify account balance
-        account = killbill.api.AccountApi()
-        account = account.get_account(account_id, random_api_key, random_api_secret, account_with_balance=True)
+        account = api_account.get_account(account_id, account_with_balance=True)
         self.assertEqual(0, account.account_balance)
 
         # Get account invoice payments
-        account = killbill.api.AccountApi()
-        invoice_payments = account.get_invoice_payments(account_id, random_api_key, random_api_secret)
+        invoice_payments = api_account.get_invoice_payments(account_id)
         payment_id = invoice_payments[0].transactions[0].payment_id
         self.assertEqual(1, len(invoice_payments[0].transactions))
 
         # Create a refund
-        payment = killbill.api.PaymentApi()
+        api_payment = killbill.api.PaymentApi()
         body = PaymentTransaction(payment_id=payment_id, amount=50)
-        payment.refund_payment(payment_id, body, 'test', random_api_key, random_api_secret)
+        api_payment.refund_payment(payment_id, body, 'test')
 
         # Get account invoice payments
-        account = killbill.api.AccountApi()
-        invoice_payments = account.get_invoice_payments(account_id, random_api_key, random_api_secret)
+        invoice_payments = api_account.get_invoice_payments(account_id)
         self.assertEqual(2, len(invoice_payments[0].transactions))
         self.assertEqual('REFUND', invoice_payments[0].transactions[1].transaction_type)
 
         # Cancel subscription
-        subscription = killbill.api.SubscriptionApi()
-        subscription.cancel_subscription_plan(subscription_id, 'test', random_api_key, random_api_secret)
+        api_subscription.cancel_subscription_plan(subscription_id, 'test')
 
         # Get subscription
-        subscription = killbill.api.SubscriptionApi()
-        subscription = subscription.get_subscription(subscription_id, random_api_key, random_api_secret)
+        subscription = api_subscription.get_subscription(subscription_id)
         self.assertEqual('CANCELLED', subscription.state)
 
         pass
@@ -214,66 +199,64 @@ class TestIntegration(unittest.TestCase):
         # Create tenant
         random_api_key = ''.join(choice(ascii_lowercase) for i in range(4))
         random_api_secret = ''.join(choice(ascii_lowercase) for i in range(5))
-        tenant = killbill.api.TenantApi()
+
+        config = Configuration()
+        config.api_key['X-Killbill-ApiKey'] = random_api_key
+        config.api_key['X-Killbill-ApiSecret'] =  random_api_secret
+
+        api_tenant = killbill.api.TenantApi()
         body = Tenant(api_key=random_api_key, api_secret=random_api_secret)
-        tenant.create_tenant(body, 'test')
+        api_tenant.create_tenant(body, 'test')
 
         # Create 10 accounts with payment methods and external charges
         for x in range(0, 10):
-            account = killbill.api.AccountApi()
+            api_account = killbill.api.AccountApi()
             random_external_key = ''.join(choice(ascii_lowercase) for i in range(6))
             body = Account(name='John-' + str(x), external_key=random_external_key, currency='USD', state='CA',
                            country='USA')
-            account.create_account(body, 'test', random_api_key, random_api_secret)
+            api_account.create_account(body, 'test')
 
             # Get account
-            account = killbill.api.AccountApi()
-            account = account.get_account_by_key(random_external_key, random_api_key, random_api_secret)
+            account = api_account.get_account_by_key(random_external_key)
             account_id = account.account_id
 
             # Add a payment method
-            account = killbill.api.AccountApi()
             body = PaymentMethod(plugin_name='__EXTERNAL_PAYMENT__', plugin_info=None)
-            account.create_payment_method(account_id, body, 'test', random_api_key, random_api_secret)
+            api_account.create_payment_method(account_id, body, 'test')
 
             # Create external charges
-            invoice = killbill.api.InvoiceApi()
+            api_invoice = killbill.api.InvoiceApi()
             body = InvoiceItem(account_id=account_id, amount=50, currency='USD', description='My charge')
-            invoice.create_external_charges(account_id, [body], 'test', random_api_key, random_api_secret,
+            api_invoice.create_external_charges(account_id, [body], 'test',
                                             auto_commit=True)
 
             # Pay all unpaid invoices
-            account = killbill.api.AccountApi()
-            account.pay_all_invoices(account_id, 'test', random_api_key, random_api_secret, external_payment=True)
+            api_account.pay_all_invoices(account_id, 'test', external_payment=True)
 
         # Pagination list accounts
-        account = killbill.api.AccountApi()
-        accounts = account.get_accounts(random_api_key, random_api_secret)
+        accounts = api_account.get_accounts()
         self.assertEqual(10, len(accounts))
 
         # Pagination list invoice
-        invoice = killbill.api.InvoiceApi()
-        invoices = invoice.get_invoices(random_api_key, random_api_secret)
+        api_invoice = killbill.api.InvoiceApi()
+        invoices = api_invoice.get_invoices()
         self.assertEqual(10, len(invoices))
 
         # Pagination list payments
-        payment = killbill.api.PaymentApi()
-        payments = payment.get_payments(random_api_key, random_api_secret)
+        api_payment = killbill.api.PaymentApi()
+        payments = api_payment.get_payments()
         self.assertEqual(10, len(payments))
 
         # Search accounts
-        account = killbill.api.AccountApi()
-        accounts = account.search_accounts('John-1', random_api_key, random_api_secret)
+        accounts = api_account.search_accounts('John-1')
         self.assertEqual(1, len(accounts))
 
         # Search payments
-        payment = killbill.api.PaymentApi()
-        payments = payment.search_payments('SUCCESS', random_api_key, random_api_secret)
+        payments = api_payment.search_payments('SUCCESS')
         self.assertEqual(10, len(payments))
 
         # Search invoices
-        invoice = killbill.api.InvoiceApi()
-        invoices = invoice.search_invoices('USD', random_api_key, random_api_secret)
+        invoices = api_invoice.search_invoices('USD')
         self.assertEqual(10, len(invoices))
 
         pass
